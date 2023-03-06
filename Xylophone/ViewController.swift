839b691b2bb102c1652fc246ebbb35947d8761e7
2thlesss <thomas.jadie.reeves@gmail.com>
2023-03-06 16:07:45 -0500

    initial commit

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,42 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var audioPlayer : AVAudioPlayer?///sets up our audio player
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
     }
-
-
+   
+        
     
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        setupAudioPlayer()
+        playSound()
+        
+        
+        func setupAudioPlayer(){
+            guard let url = Bundle.main.url(forResource: "C", withExtension: "wav") else { return }
+            do {
+                audioPlayer = try AVAudioPlayer(contentsOf: url)
+                audioPlayer?.prepareToPlay()
+            } catch {
+                print("Error loading sound file: \(error.localizedDescription)")
+                
+                
+            }
+        }
+        func playSound() {
+                audioPlayer?.play()
+            }
+    }
+    
+    
+    
+    
 }
 
